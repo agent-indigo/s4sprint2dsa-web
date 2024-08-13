6751fff05d20eb9344f07661b7a7f6ad20d6d8ee
agent-indigo <bradenhynes@gmail.com>
2024-08-13 19:32:23 -0230

    Completed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,45 @@
-const App = () => null
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+import {Provider} from 'react-redux'
+import ToastContainer from 'react-bootstrap'
+import store from './store'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import AddTreePage from './pages/AddTreePage'
+import TreePage from './pages/TreePage'
+import TreesPage from './pages/TreesPage'
+import 'bootswatch/dist/united/bootstrap.css'
+import 'react-toastify/dist/ReactToastify.css'
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route
+              path='/trees'
+              element={<TreesPage/>}
+              index={true}
+            />
+            <Route
+              path='/trees/add'
+              element={<AddTreePage/>}
+            />
+            <Route
+              path='/trees/?_id'
+              element={<TreePage/>}
+            />
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
+      <ToastContainer/>
+    </BrowserRouter>
+  </Provider>
+)
 export default App
